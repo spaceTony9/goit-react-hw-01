4eadf36e8f81d72a4109d391c7b04d0075858b20
spaceTony9 <bilokon.fw7@gmail.com>
2024-03-19 16:58:55 +0100

    Update FriendListItem.jsx

--- src/components/FriendsList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendsList/FriendListItem/FriendListItem.jsx
@@ -3,9 +3,9 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import FriendsList from '../FriendsList.jsx';
 
-const FriendListItem = ({ avatar, name, isOnline, key }) => {
+const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <div className={css.container} key={key}>
+    <div className={css.container}>
       <img className={css.img} src={avatar} alt={name} width="48" />
       <p>{name}</p>
       <p className={clsx(css.status, !isOnline ? css.offline : '')}>
@@ -17,8 +17,7 @@ const FriendListItem = ({ avatar, name, isOnline, key }) => {
 
 export default FriendListItem;
 
-FriendsList.propTypes = {
-  key: PropTypes.number.isRequired,
+FriendListItem.propTypes = {
   avatar: PropTypes.string,
   name: PropTypes.string.isRequired,
   isOnline: PropTypes.bool.isRequired,
