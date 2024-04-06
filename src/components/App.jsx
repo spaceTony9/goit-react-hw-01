0ca44cfee99c7754723cd18208eb5c8cba949a2d
spaceTony9 <bilokon.fw7@gmail.com>
2024-04-06 19:12:09 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 
 import Profile from './Profile/Profile.jsx';
--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,7 +1,5 @@
 import css from './FriendsList.module.css';
 import FriendListItem from '../FriendListItem/FriendListItem.jsx';
-import React from 'react';
-import PropTypes from 'prop-types';
 
 const FriendsList = ({ friends }) => {
   return (
